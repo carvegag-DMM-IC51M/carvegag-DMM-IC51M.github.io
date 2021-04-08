2e5392667e2af79eaf07739ab62822a66bd228dd
carvegag-DMM-IC51M <82156494+carvegag-DMM-IC51M@users.noreply.github.com>
2021-04-08 08:57:50 -0500

    Configuracion de firebase
    Configuración propia del FirebaseApp

--- js/init.js
+++ js/init.js
@@ -1,5 +1,13 @@
 // @ts-ignore
 firebase.initializeApp({
+  apiKey: "AIzaSyDe9g7WN3p-9sb84RA9aBqPK-WD-HbF-tE",
+  authDomain: "simulador-iot.firebaseapp.com",
+  projectId: "simulador-iot",
+  storageBucket: "simulador-iot.appspot.com",
+  messagingSenderId: "818151675247",
+  appId: "1:818151675247:web:a6710a6b4685263f8a449a",
+  measurementId: "G-EZR8BKV08V"
+  /*
   apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
   authDomain: "gilpgiotx.firebaseapp.com",
   databaseURL: "https://gilpgiotx.firebaseio.com",
@@ -7,4 +15,5 @@ firebase.initializeApp({
   storageBucket: "gilpgiotx.appspot.com",
   messagingSenderId: "362186415969",
   appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  */
 });
